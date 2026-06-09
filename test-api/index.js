cc77dcc0bb1f72cdce34ef4d625b2f3654009e0d
Matt Brown <mbrown@couponcabin.com>
2026-06-09 15:20:55 -0500

    First API test

---
+++ test-api/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// Define a fake route
+app.get('/', (req, res) => {
+    res.send('Welcome to the API!');
+});
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log('Server is running on http://localhost"${PORT}');
+});
