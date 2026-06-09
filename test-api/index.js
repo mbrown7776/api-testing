d1022a1c0e52e6e5445003adcba3c8343854dada
Matt Brown <mbrown@couponcabin.com>
2026-06-09 15:39:43 -0500

    Typo with backticks

--- test-api/index.js
+++ test-api/index.js
@@ -9,5 +9,5 @@ app.get('/', (req, res) => {
 // Start the server
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log('Server is running on http://localhost"${PORT}');
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
